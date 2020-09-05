e75a5411f7e0924a0755b4c6c6db259a8518d951
Mateus Borja <mateus.borja@gmail.com>
2020-09-05 18:28:21 -0400

    app component update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { provideRoutes } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'dashboard1wireframe-angular';
 }
